eb0c1dd30fc4ec6e2a8b9147929641e54ae59670
PREVOST Corentin <corentin.prevost@wildcodeschool.fr>
2020-03-17 17:51:46 +0100

    initial commit

---
+++ Notepad/Editor.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Notepad
+{
+
+    public class Editor : INotifyPropertyChanged
+    {
+        public String Text { get; set; }
+        public String FilePath
+        {
+            get => _filePath;
+            set
+            {
+                _filePath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private String _filePath;
+
+        public const String UNTITLED_FILE_PATH = "[Untitled]";
+
+        public Editor()
+        {
+            FilePath = UNTITLED_FILE_PATH;
+        }
+
+        public void SaveFileText()
+        {
+            if (!File.Exists(FilePath))
+            {
+                CreateNewTextFile(FilePath);
+                OpenFilePath(FilePath);
+            }
+
+            File.WriteAllText(FilePath, Text);
+        }
+
+        public void CreateNewTextFile(String filePath=null)
+        {
+            if (filePath == null)
+            {
+                filePath = FilePath;
+            }
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+            File.Create(filePath).Close();
+        }
+
+        public void OpenFilePath(String filePath)
+        {
+            this.FilePath = filePath;
+            if (File.Exists(FilePath))  
+            {
+                Text = File.ReadAllText(filePath);
+            }
+            else
+            {
+                Text = String.Empty;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Create the OnPropertyChanged method to raise the event
+        // The calling member's name will be used as the parameter.
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Notepad/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+using System;
+using Microsoft.Win32;
+
+namespace Notepad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Editor editor;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            editor = new Editor();
+            DataContext = editor;
+        }
+
+        private void HandleOpenFile(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                editor.OpenFilePath(openFileDialog.FileName);
+            }
+            ContentTextBox.Text = editor.Text;
+        }
+
+        private void HandleSaveFile(object sender, RoutedEventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog();
+            if (editor.FilePath == Editor.UNTITLED_FILE_PATH)
+            {
+                saveFileDialog.Title = "Save file";
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    editor.FilePath = saveFileDialog.FileName;
+                }
+                editor.CreateNewTextFile();
+            }
+            editor.SaveFileText();
+        }
+
+        private void HandleNewFile(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                editor.CreateNewTextFile(saveFileDialog.FileName);
+                editor.OpenFilePath(saveFileDialog.FileName);
+            }
+        }
+
+        private void HandleQuitApplication(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }   
+}
